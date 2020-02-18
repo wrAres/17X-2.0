d42280f0f8efb91034a2a4eccc64cdb88310325c
zzhan127 <zzhan127@ucsc.edu>
2020-02-18 14:24:13 -0800

    new scenes added

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public int isReverse;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKey("w")){
+			GetComponent<Rigidbody>().velocity = new Vector3(0,0,3*isReverse);
+		}
+		if(Input.GetKey("s")){
+			GetComponent<Rigidbody>().velocity = new Vector3(0,0,-3*isReverse);
+		}
+        if(Input.GetKey("a")){
+			GetComponent<Rigidbody>().velocity = new Vector3(-3*isReverse,0,0);
+		}
+		if(Input.GetKey("d")){
+			GetComponent<Rigidbody>().velocity = new Vector3(3*isReverse,0,0);
+		}
+    }
+}
---
+++ Assets/Scripts/sceneTransition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneTransition : MonoBehaviour
+{
+    //[SerializeField] private string newlevel;
+	public string scene;
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Player")){
+			Debug.Log("trasferring");
+			SceneManager.LoadScene(scene);
+	    }
+	}
+}
