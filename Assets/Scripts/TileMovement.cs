35a0fc67a7584051fd4c06845dfb9ed8b72062b9
Matthew <lemtolen@ucsc.edu>
2020-02-23 20:25:29 -0800

    Added in randomized moving puzzle

--- Assets/Scripts/TileMovement.cs
+++ Assets/Scripts/TileMovement.cs
@@ -3,25 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
-using UnityEngine.SceneManagement;
+
 public class TileMovement : MonoBehaviour
 {
     private Transform tileToMove;
-    private Transform invisTile; 
+    private Transform invisTile;
 
     private float speed = 100.0f;
 
     Dictionary<int, string> dict = new Dictionary<int, string>();
+    static Dictionary<Tuple<int, int>, Vector3> startPos = new Dictionary<Tuple<int, int>, Vector3>();
     int[,] Puzzle;
 
     // Start is called before the first frame update
     void Start()
     {
-        invisTile = GameObject.Find("InvisTile").GetComponent<Transform>();
-        Puzzle = MakePuzzle();
+        // Set StartPos dictionary
+        int limit = 1;
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                Vector3 tilePos = GameObject.Find("Tile" + limit++).GetComponent<Transform>().position;
+                Tuple<int, int> tup = new Tuple<int, int>(j, k);
+                startPos.Add(tup, tilePos);
+            }
+        }
 
-        //Set Dictionary to map Puzzle to screen
-        for (int i = 1; i <= 9; i++){
+        invisTile = GameObject.Find("Tile9").GetComponent<Transform>();
+        Puzzle = MakePuzzle();
+        print_puzzle();
+
+        // Set Dictionary to map Puzzle to screen
+        for (int i = 1; i <= 9; i++)
+        {
             dict.Add(i, "Tile" + i);
         }
     }
@@ -29,9 +44,11 @@ public class TileMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             Tuple<bool, string>[] tiles = GetTiles();
-            if (tiles[0].Item1 == true){
+            if (tiles[0].Item1 == true)
+            {
                 tileToMove = GameObject.Find(tiles[0].Item2).GetComponent<Transform>();
                 MoveTile(tileToMove, invisTile);
                 UpdatePuzzle("up");
@@ -39,9 +56,11 @@ public class TileMovement : MonoBehaviour
             }
             else Debug.Log("No Valid up movement");
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow)){
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
             Tuple<bool, string>[] tiles = GetTiles();
-            if (tiles[1].Item1 == true){
+            if (tiles[1].Item1 == true)
+            {
                 tileToMove = GameObject.Find(tiles[1].Item2).GetComponent<Transform>();
                 MoveTile(tileToMove, invisTile);
                 UpdatePuzzle("down");
@@ -49,9 +68,11 @@ public class TileMovement : MonoBehaviour
             }
             else Debug.Log("No valid down movement");
         }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             Tuple<bool, string>[] tiles = GetTiles();
-            if (tiles[2].Item1 == true){
+            if (tiles[2].Item1 == true)
+            {
                 tileToMove = GameObject.Find(tiles[2].Item2).GetComponent<Transform>();
                 MoveTile(tileToMove, invisTile);
                 UpdatePuzzle("left");
@@ -59,9 +80,11 @@ public class TileMovement : MonoBehaviour
             }
             else Debug.Log("No valid left movement");
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             Tuple<bool, string>[] tiles = GetTiles();
-            if (tiles[3].Item1 == true){
+            if (tiles[3].Item1 == true)
+            {
                 tileToMove = GameObject.Find(tiles[3].Item2).GetComponent<Transform>();
                 MoveTile(tileToMove, invisTile);
                 UpdatePuzzle("right");
@@ -71,39 +94,64 @@ public class TileMovement : MonoBehaviour
         }
     }
 
-    private static int[,] MakePuzzle(){
-        // Can make this static and set a puzzle
-        int[,] puzzle = new int[3,3];
-        int num = 1;
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                puzzle[i,j] = num++;
+    private static int[,] MakePuzzle()
+    {
+        // Make Puzzle Random
+        List<int> numbers = new List<int>();
+        int number;
+        for (int i = 0; i < 9; i++)
+        {
+            do
+            {
+                number = UnityEngine.Random.Range(1, 10);
+            } while (numbers.Contains(number));
+            numbers.Add(number);
+        }
+        // Place puzzle in 3x3 grid
+        int[,] puzzle = new int[3, 3];
+        int num = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                puzzle[i, j] = numbers[num];
+                GameObject tileToChange = GameObject.Find("Tile" + numbers[num++]);
+                Tuple<int, int> location = new Tuple<int, int>(i, j);
+                tileToChange.GetComponent<Transform>().position = startPos[location];
             }
         }
         return puzzle;
     }
 
-    private Tuple<bool, string>[] GetTiles(){
-        Tuple<bool, string> up    = new Tuple<bool, string>(false, "noTile");
-        Tuple<bool, string> down  = new Tuple<bool, string>(false, "noTile");
-        Tuple<bool, string> left  = new Tuple<bool, string>(false, "noTile");
+    private Tuple<bool, string>[] GetTiles()
+    {
+        Tuple<bool, string> up = new Tuple<bool, string>(false, "noTile");
+        Tuple<bool, string> down = new Tuple<bool, string>(false, "noTile");
+        Tuple<bool, string> left = new Tuple<bool, string>(false, "noTile");
         Tuple<bool, string> right = new Tuple<bool, string>(false, "noTile");
         // Find the empty tile
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (Puzzle[i,j] == 9){
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (Puzzle[i, j] == 9)
+                {
                     // Check if what moves are possible
-                    if ((i + 1) <= 2){
-                        up = new Tuple<bool, string>(true, dict[Puzzle[i+1,j]]);
+                    if ((i + 1) <= 2)
+                    {
+                        up = new Tuple<bool, string>(true, dict[Puzzle[i + 1, j]]);
                     }
-                    if ((i - 1) >= 0){
-                        down = new Tuple<bool, string>(true, dict[Puzzle[i-1,j]]);
+                    if ((i - 1) >= 0)
+                    {
+                        down = new Tuple<bool, string>(true, dict[Puzzle[i - 1, j]]);
                     }
-                    if ((j + 1) <= 2){
-                        left = new Tuple<bool, string>(true, dict[Puzzle[i,j+1]]);
+                    if ((j + 1) <= 2)
+                    {
+                        left = new Tuple<bool, string>(true, dict[Puzzle[i, j + 1]]);
                     }
-                    if ((j - 1) >= 0){
-                        right = new Tuple<bool, string>(true, dict[Puzzle[i,j-1]]);
+                    if ((j - 1) >= 0)
+                    {
+                        right = new Tuple<bool, string>(true, dict[Puzzle[i, j - 1]]);
                     }
                 }
             }
@@ -118,76 +166,90 @@ public class TileMovement : MonoBehaviour
     }
 
     // Check to see if puzzle is finished
-    private void CheckDone(){
+    private void CheckDone()
+    {
         int num = 1;
         bool flag = true;
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (Puzzle[i,j] != num++) flag = false;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (Puzzle[i, j] != num++) flag = false;
             }
         }
-        if (flag) {
-			Debug.Log("Finished Puzzle");
-			SceneManager.LoadScene("scene3");
-		}
+        if (flag) Debug.Log("Finished Puzzle");
     }
 
     // Move tile on the screen
-    private void MoveTile(Transform movingTile, Transform invTile){
+    private void MoveTile(Transform movingTile, Transform invTile)
+    {
         Vector3 Start = movingTile.position;
         Vector3 Destination = invisTile.position;
-        
-        while (Vector3.Distance(movingTile.position, Destination) > 0){
+
+        while (Vector3.Distance(movingTile.position, Destination) > 0)
+        {
             movingTile.position = Vector3.MoveTowards(movingTile.position, Destination, Time.deltaTime * speed);
         }
-        while (Vector3.Distance(invTile.position, Start) > 0){
+        while (Vector3.Distance(invTile.position, Start) > 0)
+        {
             invTile.position = Vector3.MoveTowards(invTile.position, Start, Time.deltaTime * speed);
         }
     }
 
     // Update puzzle map
-    private void UpdatePuzzle(string move){
+    private void UpdatePuzzle(string move)
+    {
         // Need this here to not move multiple times on a press for "up" and "left"
         bool movedAlready = false;
 
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                if (Puzzle[i,j] == 9){
-                    if (move == "up" && movedAlready == false){
-                        int temp = Puzzle[i,j];
-                        Puzzle[i,j] = Puzzle[i+1,j];
-                        Puzzle[i+1,j] = temp;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (Puzzle[i, j] == 9)
+                {
+                    if (move == "up" && movedAlready == false)
+                    {
+                        int temp = Puzzle[i, j];
+                        Puzzle[i, j] = Puzzle[i + 1, j];
+                        Puzzle[i + 1, j] = temp;
                         movedAlready = true;
                     }
-                    else if (move == "down"){
-                        int temp = Puzzle[i,j];
-                        Puzzle[i,j] = Puzzle[i-1,j];
-                        Puzzle[i-1,j] = temp;
+                    else if (move == "down")
+                    {
+                        int temp = Puzzle[i, j];
+                        Puzzle[i, j] = Puzzle[i - 1, j];
+                        Puzzle[i - 1, j] = temp;
                     }
-                    else if (move == "left" && movedAlready == false){
-                        int temp = Puzzle[i,j];
-                        Puzzle[i,j] = Puzzle[i,j+1];
-                        Puzzle[i,j+1] = temp;
+                    else if (move == "left" && movedAlready == false)
+                    {
+                        int temp = Puzzle[i, j];
+                        Puzzle[i, j] = Puzzle[i, j + 1];
+                        Puzzle[i, j + 1] = temp;
                         movedAlready = true;
                     }
-                    else if (move == "right"){
-                        int temp = Puzzle[i,j];
-                        Puzzle[i,j] = Puzzle[i,j-1];
-                        Puzzle[i,j-1] = temp;
+                    else if (move == "right")
+                    {
+                        int temp = Puzzle[i, j];
+                        Puzzle[i, j] = Puzzle[i, j - 1];
+                        Puzzle[i, j - 1] = temp;
                     }
                 }
             }
         }
     }
-    
+
 
     // Debug methods delete later...
 
-    private void print_puzzle(){
+    private void print_puzzle()
+    {
         string toPrint = "";
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                toPrint = toPrint + Puzzle[i,j] + ", ";
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                toPrint = toPrint + Puzzle[i, j] + ", ";
             }
             Debug.Log(toPrint);
             toPrint = "\n\t    ";
