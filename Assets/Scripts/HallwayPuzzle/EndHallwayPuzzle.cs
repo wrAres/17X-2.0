6a3ffdb7313ac0e0bb61165639022f52e2fe86ac
Matthew <lemtolen@ucsc.edu>
2020-02-25 20:19:15 -0800

    Added in hallway puzzle

---
+++ Assets/Scripts/HallwayPuzzle/EndHallwayPuzzle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndHallwayPuzzle : MonoBehaviour
+{
+    public FloorControl bounds;
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        bounds.start = false;
+        bounds.end = true;
+    }
+}
