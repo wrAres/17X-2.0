814723ffb2c4290025b0d7158a0e484f11f22050
Matthew <lemtolen@ucsc.edu>
2020-03-03 13:43:17 -0800

    Fixed falling from walking puzzle

--- Assets/Scripts/HallwayPuzzle/EndHallwayPuzzle.cs
+++ Assets/Scripts/HallwayPuzzle/EndHallwayPuzzle.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class EndHallwayPuzzle : MonoBehaviour
 {
-    public FloorControl bounds;
+    private Vector3 startPos;
 
-    private void OnTriggerEnter(Collider obj)
+    private GameObject MainChar;
+
+    void Start()
     {
-        bounds.start = false;
-        bounds.end = true;
+        GameObject MainChar = GameObject.Find("Main Character");
+        startPos = MainChar.GetComponent<Transform>().position;
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("Main Character").GetComponent<Transform>().position.y < .6) 
+        {
+            GameObject.Find("Main Character").GetComponent<Transform>().position = startPos; 
+        }
     }
 }
