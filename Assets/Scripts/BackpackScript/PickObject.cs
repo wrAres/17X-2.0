0831654cbde09e4dc6e98f3a1873d27a3da9ee1c
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2020-03-02 17:28:20 -0800

    water seed pickable but bug with end walking puzzle piece

--- Assets/Scripts/BackpackScript/PickObject.cs
+++ Assets/Scripts/BackpackScript/PickObject.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class PickObject : MonoBehaviour
 {   
     private int layerMask;
-    private GameObject[] pickableList;
 
     private void Start() {
         // This would cast rays only against colliders in layer 8.
         // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        print("start");
         layerMask = 1 << 8;
         layerMask = ~layerMask;
     }
@@ -17,10 +17,12 @@ public class PickObject : MonoBehaviour
     // Update is called once per frame
     void Update(){
         if (Input.GetMouseButtonDown(0)) {
+            print("aaaa");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity, layerMask)) {
                 GameObject clickObject = hitInfo.collider.gameObject;
+                print(clickObject.name);
                 if (clickObject.tag.CompareTo("Pickable") == 0){
                     Backpack.backpack.GetComponent<Backpack>().AddItem(clickObject.name);
                     Destroy(clickObject);
