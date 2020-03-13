8e5eead38942de2224d04aa6bc0a5da6975e4798
许华真 <xuhuazhen@xuhuazhendeMacBook-Air.local>
2020-03-12 19:26:44 -0700

    object tips dialog

---
+++ Assets/Scripts/TipsDialog.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TipsDialog : MonoBehaviour
+{
+    public GameObject dialog;
+
+    private void OnCollisionEnter(Collision collision){
+     if(collision.gameObject.tag == "Player"){
+      dialog.SetActive(true);
+     }
+    }
+    private void OnCollisionExit(Collision collision){
+     if(collision.gameObject.tag == "Player"){
+      dialog.SetActive(false);
+     }
+    }
+}
