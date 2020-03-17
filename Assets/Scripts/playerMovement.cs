354f05366798101b11b6d8d455121d1027eb3722
zzhan127 <zzhan127@ucsc.edu>
2020-03-17 02:20:22 -0700

    wind and pool implemented

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -18,10 +18,12 @@ public class playerMovement : MonoBehaviour
 		
 		if(GetComponent<Rigidbody>().transform.position.y < -20){
 			GetComponent<Rigidbody>().transform.position = new Vector3(0,2,-43);
+			
 		}
 		ani.SetFloat("Speed", GetComponent<Rigidbody>().velocity.z);
 		if(Input.GetKey("w")){
 			GetComponent<Rigidbody>().velocity = new Vector3(0,0,3*isReverse);
+			
 		}
 		if(Input.GetKey("s")){
 			GetComponent<Rigidbody>().velocity = new Vector3(0,0,-3*isReverse);
