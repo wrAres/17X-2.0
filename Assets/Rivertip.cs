d1bd2150ce1f6ffe1af56f8a4e92964fb64ef9c7
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2020-03-30 02:01:00 -0700

    talisman 1 auto tip

--- Assets/Rivertip.cs
+++ Assets/Rivertip.cs
@@ -9,4 +9,13 @@ public class Rivertip : MonoBehaviour
     {
         gameObject.GetComponent<Renderer>().enabled = false;
     }
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Main Character")
+        {
+            this.gameObject.GetComponent<CapsuleCollider>().radius = 0;
+            TipsDialog.PrintDialog("Talisman 1");
+        }
+    }
 }
