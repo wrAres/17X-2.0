3e401654c8929ee1274b45c334fc3b5b12cefd8f
“xuhuarzhen” <hxu49@ucsc.edu>
2020-04-11 22:59:57 -0700

    add mainmenu

---
+++ Assets/Scripts/UICanvas/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+     SceneManager.LoadScene("Tutorial");
+    }
+
+    public void QuitGame(){
+     UnityEditor.EditorApplication.isPlaying = false;
+     Debug.Log("quit game");
+     Application.Quit();
+    }
+}
