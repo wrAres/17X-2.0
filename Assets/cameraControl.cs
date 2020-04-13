e753a24126c30fe58f3e580959971d88d3d1fdfe
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-04-12 22:02:07 -0700

    waterRoom camera zoom function added

---
+++ Assets/cameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public Camera def;
+	public Camera zoomed;
+	AudioListener CameraDefAudio;
+	AudioListener CameraZoomedAudio;
+    void Start()
+    {
+        def.enabled = true;
+		zoomed.enabled = false;
+		CameraDefAudio = def.GetComponent<AudioListener>();
+		CameraZoomedAudio = zoomed.GetComponent<AudioListener>();
+		CameraDefAudio.enabled = true;
+		CameraZoomedAudio.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		
+        if (Input.GetKeyDown("z")) {
+			def.enabled = !def.enabled;
+			zoomed.enabled = !zoomed.enabled;
+		}
+    }
+}
---
+++ Assets/mirrorCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirrorCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().transform.position = new Vector3(-3,2,-14);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        if (Input.GetKey("left"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-3, 0, 0);
+        }
+		if (Input.GetKey("right"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(3, 0, 0);
+        }
+		if(Input.GetKey("up")){
+			GetComponent<Rigidbody>().transform.position = new Vector3(-9,2,-10);
+		}
+		if(Input.GetKey("down")){
+			GetComponent<Rigidbody>().transform.position = new Vector3(-3,2,-14);
+		}
+    }
+}
