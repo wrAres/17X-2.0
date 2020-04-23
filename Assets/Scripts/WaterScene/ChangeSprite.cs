f6eea056fb97f62e6def8085db1a3358c312659a
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2020-04-22 18:29:45 -0700

    scroll gate collider change when open

--- Assets/Scripts/WaterScene/ChangeSprite.cs
+++ Assets/Scripts/WaterScene/ChangeSprite.cs
@@ -13,6 +13,8 @@ public class ChangeSprite : MonoBehaviour
     public void OpenScroll() {
         Trigger = true;
         this.gameObject.GetComponent<SpriteRenderer>().sprite = Target;
+        Vector3 size = this.gameObject.GetComponent<BoxCollider>().size;
+        this.gameObject.GetComponent<BoxCollider>().size = new Vector3(17f, size.y, size.z);
         string name = this.gameObject.name;
         sceneTransition transition = this.gameObject.GetComponent<sceneTransition>();
         transition.CheckOpen();
