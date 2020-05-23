df955fb05e5bde88671e71162f2e5b8284d77577
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-05-23 00:39:01 -0700

    water animation, door angle

---
+++ Assets/EarthGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter(Collision collision){
+		// Debug.Log("collide!");
+		if(collision.gameObject.tag == "Player"){
+			//collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+			collision.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY|
+																			RigidbodyConstraints.FreezeRotationX|
+																			RigidbodyConstraints.FreezeRotationZ|
+																			RigidbodyConstraints.FreezeRotationY;
+		}
+		
+	}
+}
