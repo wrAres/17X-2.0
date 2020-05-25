001855db645fbcdc49d32f5c466346847b1c3312
Matthew <lemtolen@ucsc.edu>
2020-05-25 07:49:25 -0700

    Added in push objects

--- Assets/Scripts/Player/pushPlayer.cs
+++ Assets/Scripts/Player/pushPlayer.cs
@@ -4,23 +4,73 @@ using UnityEngine;
 
 public class pushPlayer : MonoBehaviour
 {
+    private GameObject ItemtoPush = null;
+
+    private bool avaliblePush = false;
+    private bool currentlyPush = false;
+    private Transform tempTransform;
+
+    public PushHud Hud;
+
+    private string[] pushItems;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pushItems = new string[] { "Rock1", "Rock2", "Rock3", "Rock4", "Rock5", "Rock6", "Rock7", "Rock8", "Rock9", "Rock10"};
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ItemtoPush != null && Input.GetKeyDown(KeyCode.E) && avaliblePush)
+        {
+            currentlyPush = true;
+            ItemtoPush.transform.parent = transform;
+            Debug.Log("Made " + ItemtoPush.name + " Child of me");
+        }
+        if (Input.GetKeyUp(KeyCode.E) && ItemtoPush != null)
+        {
+            currentlyPush = false;
+            ItemtoPush.transform.parent = tempTransform;
+        }
     }
-	void OnCollisionEnter(Collision collision){
-		// Debug.Log("collide!");
-		if(collision.gameObject.tag == "Player"){
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("Collide with" + collisionInfo.collider.name);
+        Debug.Log("cuurpush" + currentlyPush);
+        if (!currentlyPush)
+        {
+            Debug.Log(collisionInfo.collider.name);
+            //ItemtoPush = collisionInfo.collider.gameObject;
+            ItemtoPush = collisionInfo.collider.gameObject.transform.parent.gameObject;
+            tempTransform = ItemtoPush.transform.parent; 
+            foreach (string item in pushItems)
+            {
+                if (ItemtoPush.name == item)
+                {
+                    avaliblePush = true;
+                    Debug.Log("Push is: " + avaliblePush);
+                }
+            }
+        }
+        Hud.OpenMessagePanel(collisionInfo.collider.name);
+        Debug.Log("ShowPanel");
+        /*if(collision.gameObject.tag == "Player"){
 			//collision.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
 			collision.gameObject.GetComponent<playerMovement>().pushBack();
-		}
-		
-	}
+		}*/
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        Hud.CloseMessagePanel();
+        avaliblePush = false;
+        Debug.Log("Push is: " + avaliblePush);
+        //ItemtoPush.transform.parent = tempTransform;
+        ItemtoPush = null;
+
+    }
 }
---
+++ Assets/Scripts/UICanvas/PushHud.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushHud : MonoBehaviour
+{
+    public GameObject MessagePanel;
+    public Text PushMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MessagePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMessagePanel(string item)
+    {
+        PushMessage.text = "Hold E to move " + item;
+        MessagePanel.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        MessagePanel.SetActive(false);
+    }
+}
