3e6d301fa0655e0c1ece625f1744bc96062c3da3
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-05-24 20:30:39 -0700

    water scroll now can enter from all direection

--- Assets/Scripts/Scroll Gates/ChangeSprite.cs
+++ Assets/Scripts/Scroll Gates/ChangeSprite.cs
@@ -7,7 +7,7 @@ public class ChangeSprite : MonoBehaviour
     public Sprite Target;
     public bool Trigger = false;
     // Start is called before the first frame update
-
+	public GameObject trans;
 
     // Update is called once per frame
     public void OpenScroll() {
@@ -18,7 +18,7 @@ public class ChangeSprite : MonoBehaviour
         size = this.gameObject.transform.GetChild(0).GetComponent<BoxCollider>().size;
         this.gameObject.transform.GetChild(0).GetComponent<BoxCollider>().size = new Vector3(17f, size.y, size.z);
         string name = this.gameObject.name;
-        sceneTransition transition = this.gameObject.GetComponent<sceneTransition>();
+        sceneTransition transition = trans.gameObject.GetComponent<sceneTransition>();
         UISoundScript.OpenTalisman(); // Here for open scroll gate
         transition.CheckOpen();
     }
--- Assets/Scripts/Scroll Gates/sceneTransition.cs
+++ Assets/Scripts/Scroll Gates/sceneTransition.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class sceneTransition : MonoBehaviour
 {
     //[SerializeField] private string newlevel;
 	public string scene;
 	public bool enterable;
 	public bool openScroll;
-
+	public Text myText;
 	void Start() {
 		openScroll = false;
 		enterable = false;
 		if (this.gameObject.name.CompareTo("WaterToEarthPortal") == 0) {
 			enterable = true;
 		}
+		myText.text = "";
 	}
 
 	public void CheckOpen() {
@@ -30,11 +32,13 @@ public class sceneTransition : MonoBehaviour
 
 		if (this.gameObject.name.CompareTo("法阵-scene3") == 0 && DontDestroyVariables.enterWaterRoom) {
 			// print("scene 3 directly");
+			//myText.text = "Loading";
 			scene = "scene3";
 		}
 
 		if (other.CompareTag("Player") && enterable) {
 			// Debug.Log("trasferring");
+			//myText.text = "Loading";
 			SceneManager.LoadScene(scene);
 		}
 	}
