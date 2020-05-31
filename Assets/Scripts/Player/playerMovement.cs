1a371f082af47d8c9ca82aad4e01008444768f0a
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-05-30 20:37:34 -0700

    loading screen fix

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -14,7 +14,7 @@ public class playerMovement : MonoBehaviour
 	public int freeze;
 	private bool isWaterScene =>
         SceneManager.GetActiveScene().name == "scene3";
-	public bool canAct => !dialogShown && !talismanShown && !shineIcon && camera.enabled &&!events&&!fall;
+	public bool canAct => /*!dialogShown && !talismanShown && !shineIcon && camera.enabled &&*/!events&&!fall;
 	public bool dialogShown =>
         FindObjectOfType<TipsDialog>() != null;
 	public bool talismanShown =>
--- Assets/castEffect.cs
+++ Assets/castEffect.cs
@@ -6,30 +6,36 @@ public class castEffect : MonoBehaviour
 {
     // Start is called before the first frame update
 	public bool cast;
-	int timer;
+	//int timer;
 	int start;
 	public Animator ani;
     void Start()
     {
         cast = false;
-		timer = 0;
+		//timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
 		ani.SetBool("cast",cast);
-		timer++;
-        if(start +140<=timer && cast ==true){
+		//timer++;
+        /*if(start +140<=timer && cast ==true){
 			cast = false;
-		}
+		}*/
 		if (Input.GetKey("p")){
 			castAni();
+		}
+		if (Input.GetKey("t")){
+			stopCasting();
 		}
     }
 	
 	void castAni(){
-		start = timer;
+		//start = timer;
 		cast = true;
+	}
+	void stopCasting(){
+		cast = false;
 	}
 }
