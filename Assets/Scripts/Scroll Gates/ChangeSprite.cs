af254618ab688d8382a0d00b3c9321596a73ad84
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-05-31 00:41:05 -0700

    scroll bug fix

--- Assets/Scripts/Scroll Gates/ChangeSprite.cs
+++ Assets/Scripts/Scroll Gates/ChangeSprite.cs
@@ -8,7 +8,13 @@ public class ChangeSprite : MonoBehaviour
     public bool Trigger = false;
     // Start is called before the first frame update
 	public GameObject trans;
-
+	void Update(){
+		/*if(Input.GetKey("o")){
+			OpenScroll();
+		}*/
+		
+		
+	}
     // Update is called once per frame
     public void OpenScroll() {
         Trigger = true;
@@ -19,7 +25,8 @@ public class ChangeSprite : MonoBehaviour
         this.gameObject.transform.GetChild(0).GetComponent<BoxCollider>().size = new Vector3(17f, size.y, size.z);
         string name = this.gameObject.name;
         sceneTransition transition = trans.gameObject.GetComponent<sceneTransition>();
+		transition.CheckOpen();
         UISoundScript.OpenTalisman(); // Here for open scroll gate
-        transition.CheckOpen();
+        
     }
 }
