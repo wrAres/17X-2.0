5aab7e2d83dfd32ff35be18a5e56ec966737b8bf
Zhengyang Zhang <zzhan127@ucsc.edu>
2020-06-08 02:49:46 -0700

    lockOnGround script

---
+++ Assets/Scripts/misc/lockOnGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockOnGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter(Collision collision){
+		this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY|
+																			RigidbodyConstraints.FreezeRotationX|
+																			RigidbodyConstraints.FreezeRotationZ|
+																			RigidbodyConstraints.FreezePositionX|
+																			RigidbodyConstraints.FreezePositionZ|
+																			RigidbodyConstraints.FreezeRotationY;
+	}
+}
