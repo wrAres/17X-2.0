12fcfc1d786ec7663dd9c1f314506381ca590549
Liuwenda (Winston) Gao <lgao9@ucsc.edu>
2020-06-09 12:53:07 -0700

    replace button assets

---
+++ Assets/Scripts/UICanvas/Menu/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler 
+{
+    private GameObject buttonImage;
+
+    public void Start() {
+        buttonImage = this.gameObject.transform.GetChild(0).gameObject;
+    }
+    public void OnPointerEnter(PointerEventData pointerEventData) {
+        buttonImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("UI/Menu/" + buttonImage.name + "_Shadow");
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData) {
+        buttonImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("UI/Menu/" + buttonImage.name);
+    }
+
+}
